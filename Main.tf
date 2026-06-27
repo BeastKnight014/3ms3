410bab782515fc1492cf293945cbe3ff11179e94
BeastKnight014 <darkbeastknight014@gmail.com>
2026-06-27 11:38:39 +0530

    Create Main.tf

---
+++ Main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "samruddhi-terraform-state-bucket"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "samruddhi-terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform Lock Table"
+  }
+}
